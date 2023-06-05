f8ccd49ec3c8a23e19488c9d6c7951b52a4afae0
Zanin Daniil <etu.zanin@yandex.ru>
2023-06-05 03:55:14 +0300

    first commit
    добавил первый коммит

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+apt-get -y update
+apt install amass -y
+apt install dnsutils -y
+apt-get install nmap -y
+apt install nuclei -y
+apt-get install git -y
+git clone https://github.com/projectdiscovery/nuclei-templates.git
